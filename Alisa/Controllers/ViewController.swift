5a4a11cd2d4e7191519da77425539638b74b808d
Sergii_Lisnyi <Sergii_Lisnyi@epam.com>
2018-11-15 15:01:15 +0200

    first commit

---
+++ Alisa/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Alisa
+//
+//  Created by Sergii Lisnyi on 11/15/18.
+//  Copyright © 2018 Sergii Lisnyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
